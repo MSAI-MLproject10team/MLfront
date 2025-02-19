5900d601f1dd025c69fb637a47770ad1736e25b3
suyeon0702 <42757784+suyeon0702@users.noreply.github.com>
2025-02-19 12:37:13 +0900

    Add files via upload
    Upload App Color Dict

---
+++ lib/colors.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // 🌟 메인 컬러
+  static const Color primary = Color(0xFFD6EAF8); // 연한 하늘색
+  static const Color primaryAccent = Color(0xFF87CCFD); // 메인 보조 컬러
+  static const Color grey = Color(0xFFC0C0C0); // 연회색
+  static const Color white = Color(0xFFFFFFFF); // 흰색
+  static const Color black = Color(0xFF000000); // 검은색
+
+  // 🌸 Spring (봄) 톤
+  static const Color springLightFont = Color(0xFFFBD931);
+  static const Color springLightBg = Color(0xFFFFF5C2);
+  static const Color springBrightFont = Color(0xFFF06260);
+  static const Color springBrightBg = Color(0xFFFFC5C5);
+  static const Color springWarmFont = Color(0xFFFDA844);
+  static const Color springWarmBg = Color(0xFFFFD9AC);
+
+  // ❄️ Summer (여름) 톤
+  static const Color summerLightFont = Color(0xFF84CAEC);
+  static const Color summerLightBg = Color(0xFFC2EBFF);
+  static const Color summerMuteFont = Color(0xFF8977B5);
+  static const Color summerMuteBg = Color(0xFFC2B4E2);
+  static const Color summerCoolFont = Color(0xFF346EA0);
+  static const Color summerCoolBg = Color(0xFF9BCFFC);
+
+  // 🍂 Autumn (가을) 톤
+  static const Color autumnDeepFont = Color(0xFF442129);
+  static const Color autumnDeepBg = Color(0xFFA8878F);
+  static const Color autumnMuteFont = Color(0xFF86956D);
+  static const Color autumnMuteBg = Color(0xFFB3C397);
+  static const Color autumnWarmFont = Color(0xFFB4926D);
+  static const Color autumnWarmBg = Color(0xFFDCBD9C);
+
+  // ❄️ Winter (겨울) 톤
+  static const Color winterDeepFont = Color(0xFF17383F);
+  static const Color winterDeepBg = Color(0xFF519AA9);
+  static const Color winterBrightFont = Color(0xFF1FA793);
+  static const Color winterBrightBg = Color(0xFF89F6E6);
+  static const Color winterCoolFont = Color(0xFFFFFFFF);
+  static const Color winterCoolBg = Color(0xFF302221);
+}
